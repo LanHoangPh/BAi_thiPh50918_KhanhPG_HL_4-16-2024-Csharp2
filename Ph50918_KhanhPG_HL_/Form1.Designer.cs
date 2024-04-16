77dfabc8ea1040934cc60ff945eb818582ec7930
Lenovo <Lenovo@DESKTOP-G1OA6S5>
2024-04-16 19:42:21 +0700

    Add project files.

---
+++ Ph50918_KhanhPG_HL_/Form1.Designer.cs
@@ -0,0 +1,338 @@
+﻿namespace Ph50918_KhanhPG_HL_
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            label2 = new Label();
+            label1 = new Label();
+            button4 = new Button();
+            comboBox1 = new ComboBox();
+            textBox2 = new TextBox();
+            button3 = new Button();
+            groupBox2 = new GroupBox();
+            textBox1 = new TextBox();
+            button2 = new Button();
+            button1 = new Button();
+            groupBox3 = new GroupBox();
+            radioButton3 = new RadioButton();
+            radioButton2 = new RadioButton();
+            radioButton1 = new RadioButton();
+            label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            dataGridView1 = new DataGridView();
+            textBox4 = new TextBox();
+            textBox3 = new TextBox();
+            button6 = new Button();
+            button5_load = new Button();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Controls.Add(button4);
+            groupBox1.Controls.Add(comboBox1);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(button3);
+            groupBox1.Location = new Point(12, 4);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(657, 274);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Câu1";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(64, 178);
+            label2.Name = "label2";
+            label2.Size = new Size(57, 20);
+            label2.TabIndex = 5;
+            label2.Text = "Địa Chỉ";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(64, 90);
+            label1.Name = "label1";
+            label1.Size = new Size(42, 20);
+            label1.TabIndex = 4;
+            label1.Text = "Tuổi ";
+            // 
+            // button4
+            // 
+            button4.Location = new Point(116, 30);
+            button4.Name = "button4";
+            button4.Size = new Size(94, 54);
+            button4.TabIndex = 3;
+            button4.Text = "Thông báo";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(64, 212);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(478, 28);
+            comboBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(64, 122);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(478, 27);
+            textBox2.TabIndex = 1;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(431, 30);
+            button3.Name = "button3";
+            button3.Size = new Size(94, 54);
+            button3.TabIndex = 0;
+            button3.Text = "Load";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(textBox1);
+            groupBox2.Controls.Add(button2);
+            groupBox2.Controls.Add(button1);
+            groupBox2.Location = new Point(12, 284);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(657, 320);
+            groupBox2.TabIndex = 1;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Câu 2";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(22, 147);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(618, 167);
+            textBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(383, 77);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 1;
+            button2.Text = "Ghi";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(135, 77);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 0;
+            button1.Text = "Đọc";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(radioButton3);
+            groupBox3.Controls.Add(radioButton2);
+            groupBox3.Controls.Add(radioButton1);
+            groupBox3.Controls.Add(label5);
+            groupBox3.Controls.Add(label4);
+            groupBox3.Controls.Add(label3);
+            groupBox3.Controls.Add(dataGridView1);
+            groupBox3.Controls.Add(textBox4);
+            groupBox3.Controls.Add(textBox3);
+            groupBox3.Controls.Add(button6);
+            groupBox3.Controls.Add(button5_load);
+            groupBox3.Location = new Point(675, 4);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(427, 600);
+            groupBox3.TabIndex = 2;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "Câu 3";
+            // 
+            // radioButton3
+            // 
+            radioButton3.AutoSize = true;
+            radioButton3.Location = new Point(321, 288);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new Size(62, 24);
+            radioButton3.TabIndex = 10;
+            radioButton3.TabStop = true;
+            radioButton3.Text = "Khác";
+            radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(213, 288);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(58, 24);
+            radioButton2.TabIndex = 9;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "20%";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(115, 288);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(58, 24);
+            radioButton1.TabIndex = 8;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "10%";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(6, 290);
+            label5.Name = "label5";
+            label5.Size = new Size(78, 20);
+            label5.TabIndex = 7;
+            label5.Text = "Mức Thuế ";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(39, 201);
+            label4.Name = "label4";
+            label4.Size = new Size(31, 20);
+            label4.TabIndex = 6;
+            label4.Text = "Giá";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(40, 106);
+            label3.Name = "label3";
+            label3.Size = new Size(30, 20);
+            label3.TabIndex = 5;
+            label3.Text = "Mã";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(6, 331);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(415, 263);
+            dataGridView1.TabIndex = 4;
+            dataGridView1.CellClick += dataGridView1_CellClick;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(40, 236);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(354, 27);
+            textBox4.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(40, 142);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(354, 27);
+            textBox3.TabIndex = 2;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(272, 55);
+            button6.Name = "button6";
+            button6.Size = new Size(94, 38);
+            button6.TabIndex = 1;
+            button6.Text = "Clear";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // button5_load
+            // 
+            button5_load.Location = new Point(79, 54);
+            button5_load.Name = "button5_load";
+            button5_load.Size = new Size(94, 39);
+            button5_load.TabIndex = 0;
+            button5_load.Text = "Load";
+            button5_load.UseVisualStyleBackColor = true;
+            button5_load.Click += button5_load_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1109, 616);
+            Controls.Add(groupBox3);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Name = "Form1";
+            Text = "Form1";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            groupBox3.ResumeLayout(false);
+            groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private Label label2;
+        private Label label1;
+        private Button button4;
+        private ComboBox comboBox1;
+        private TextBox textBox2;
+        private Button button3;
+        private GroupBox groupBox2;
+        private TextBox textBox1;
+        private Button button2;
+        private Button button1;
+        private GroupBox groupBox3;
+        private Button button6;
+        private Button button5_load;
+        private RadioButton radioButton3;
+        private RadioButton radioButton2;
+        private RadioButton radioButton1;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private DataGridView dataGridView1;
+        private TextBox textBox4;
+        private TextBox textBox3;
+    }
+}
---
+++ Ph50918_KhanhPG_HL_/Form1.cs
@@ -0,0 +1,134 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Data.SqlClient;
+using System.Data;
+using System.Drawing;
+using System.Collections.Generic;
+namespace Ph50918_KhanhPG_HL_
+{
+    public partial class Form1 : Form
+    {
+        string connectionString;
+        public Form1()
+        {
+            connectionString = @"Data Source=DESKTOP-G1OA6S5\HOANGLAN;Initial Catalog=NET1021_Final33;Integrated Security=True;TrustServerCertificate=True";
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Title = " Vui lòng chọn đúng File để mở";
+            openFileDialog.Filter = "Tệp tin văn bản (*.txt)|*.txt| tất cả các tệp tin(*.*)|*.*";
+            openFileDialog.ShowDialog();
+            string path = openFileDialog.FileName;
+            textBox1.Text = File.ReadAllText(path);
+            MessageBox.Show("Đọc thành công");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            string textdata = textBox1.Text;
+            saveFileDialog.Filter = "tệp tin văn bản (*.txt)|*.txt";
+            saveFileDialog.ShowDialog();
+            string filepath = saveFileDialog.FileName;
+            DialogResult result = MessageBox.Show("BẠN CÓ MUỐN LƯU TRỮ VÀO FILE HAY KO", "Xác nhận", MessageBoxButtons.YesNo);
+            if (result == DialogResult.Yes)
+            {
+                File.WriteAllText(filepath, textdata);
+                MessageBox.Show("Lưu thành công");
+            }
+            else MessageBox.Show("Tiếp tục nhập");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int tuoi;
+            if (int.TryParse(textBox2.Text, out tuoi))
+            {
+                string thongBao;
+
+                if (tuoi >= 0 && tuoi <= 10)
+                {
+                    thongBao = "Thiếu nhi";
+                }
+                else if (tuoi >= 11 && tuoi <= 18)
+                {
+                    thongBao = "Thanh thiếu niên";
+                }
+                else if (tuoi >= 19 && tuoi <= 60)
+                {
+                    thongBao = "Tuổi trưởng thành";
+                }
+                else if (tuoi > 60)
+                {
+                    thongBao = "Lão niên";
+                }
+                else
+                {
+                    thongBao = "Tuổi không hợp lệ";
+                }
+
+                MessageBox.Show($"Tuổi: {tuoi}\n{thongBao}");
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng nhập một số hợp lệ.");
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add("Thái bình");
+            comboBox1.Items.Add("Hà Nội");
+            comboBox1.Items.Add("Quảng ninh");
+            comboBox1.Items.Add("HƯng yên");
+            comboBox1.Items.Add("Hà Nam");
+        }
+
+        private void button5_load_Click(object sender, EventArgs e)
+        {
+            using SqlConnection conn = new SqlConnection(connectionString);
+
+            conn.Open();
+            string query = "SELECT * FROM Mathang";
+            SqlCommand command = new SqlCommand(query, conn); ;
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            dataGridView1.DataSource = dt;
+            conn.Close();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = "";
+            textBox4.Text = "";
+            radioButton3.Checked = true;
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {            
+                DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
+                textBox3.Text = row.Cells["ten"].Value.ToString();
+                textBox4.Text = row.Cells["gia"].Value.ToString();
+                string mucthue = row.Cells["mucthue"].Value.ToString();
+                if (mucthue == "10%")
+                {
+                    radioButton1.Checked = true;
+                }
+                else if (mucthue == "20%")
+                {
+                    radioButton2.Checked = true;
+                }
+                else
+                {
+                    radioButton3.Checked = true;
+                }
+            }
+        }
+    }
+}
